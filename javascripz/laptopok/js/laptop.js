c8591f6f4b961537910d146360cea91183705fff
zamboilles21 <zamboilles@turr.hu>
2022-09-29 12:03:36 +0200

    update2

--- javascripz/laptopok/js/laptop.js
+++ javascripz/laptopok/js/laptop.js
@@ -5,7 +5,11 @@ class Tlaptop{
     description;
     price;
 
-    constructor(){
-        
+    constructor(data){
+        this.ID=data.ID;
+        this.brand=data.brand;
+        this.type=data.type;
+        this.description=data.description;
+        this.price=data.price;
     }
 }
