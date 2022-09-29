396dc2f7c3d4b4efc58d632cad59c7a2f3fbd8f6
zamboilles21 <zamboilles@turr.hu>
2022-09-29 11:34:09 +0200

    sdasd

--- javascripz/OOP1/points.js
+++ javascripz/OOP1/points.js
@@ -1,15 +1,20 @@
 class Tpont {
-    x
-    y
+    x;
+    y;
+    distance;
 
     constructor(x, y) {
-        this.x = x
-        this.y = y
+        this.x = x;
+        this.y = y;
+        
     }
+    
 
     randomGen(min, max) {
         this.x = Math.round((Math.random() * (max - min)) + min)
         this.y = Math.round((Math.random() * (max - min)) + min)
+        this.distance = Math.sqrt(this.x * this.x + this.y * this.y);
+    
     }
-
+    
 }
