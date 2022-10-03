b49475b2f46bc01da07f8cf11947fa60dc72310d
zamboilles21 <zamboilles@turr.hu>
2022-10-03 14:00:07 +0200

    fulll

--- javascripz/users/server/server.js
+++ javascripz/users/server/server.js
@@ -3,7 +3,13 @@ const express=require('express');
 const app=express();
 
 app.get('/users', (req,res)=>{
-    
+    fs.readFile('data/useres.json', (err,data)=>{
+        if (err) {
+            res.send(err)
+        }else{
+            res.send(data)
+        }
+    });
 });
 
 app.listen(8081, ()=>{
