e0074db82c77424f338dc2c2eb62704dea72d275
zamboilles21 <zamboilles@turr.hu>
2022-10-03 13:52:26 +0200

    users

---
+++ javascripz/users/server/server.js
@@ -0,0 +1,11 @@
+const fs=require('fs');
+const express=require('express');
+const app=express();
+
+app.get('/users', (req,res)=>{
+    
+});
+
+app.listen(8081, ()=>{
+    console.log(`életjel`);
+});
