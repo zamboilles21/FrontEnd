698dc1db5e26496de22990b28257ebf19e024a21
zamboilles21 <zamboilles@turr.hu>
2022-10-10 14:41:28 +0200

    aaaa

--- angular/telefonkonyv/js/telefonkonyv.js
+++ angular/telefonkonyv/js/telefonkonyv.js
@@ -8,10 +8,18 @@ app.controller('mainCtrl', function($scope){
     $scope.ujadat={};
 
     $scope.adatok=angular.fromJson(window.localStorage.getItem('telefonkonyv'));
-
+    if($scope.adatok==null){
+        $scope.adatok=[];
+    }
     $scope.hozzaadas=function(){
-        $scope.adatok.push({nev:$scope.ujadat.nev,telszam:$scope.ujadat.telszam,kategoria:$scope.ujadat.kategoria});
-        window.localStorage.setItem('telefonkonyv', angular.toJson($scope.adatok));
+        if ($scope.ujadat.nev == '' || $scope.ujadat.telszam == '' || $scope.ujadat.kategoria =='' ) {
+            $scope.message='Nincs minden adat megadva';
+        }else{
+            $scope.adatok.push({nev:$scope.ujadat.nev,telszam:$scope.ujadat.telszam,kategoria:$scope.ujadat.kategoria});
+            window.localStorage.setItem('telefonkonyv', angular.toJson($scope.adatok));
+            $scope.ujadat={};
+        }
+        
     }
 
 });
