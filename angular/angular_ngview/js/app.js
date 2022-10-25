343d76a5f7ae30e2f0a23ac050fc88707fdd4e5f
zamboilles21 <zamboilles@turr.hu>
2022-10-25 12:48:07 +0200

    feltolt

--- angular/angular_ngview/js/app.js
+++ angular/angular_ngview/js/app.js
@@ -1,46 +1,54 @@
 let app = new angular.module('kerteszet', ['ngRoute']);
 
-app.run(function($rootScope){
-    $rootScope.title='AngularJS ngView';
-    $rootScope.author='2/14 SZFT';
-    $rootScope.company='Türr'
+app.run(function($rootScope) {
+    $rootScope.title = 'AngularJS ngView példa';
+    $rootScope.author = '2/14. szoftverfejlesztő';
+    $rootScope.company = 'Bajai SZC Türr István Technikum';
 
-    $rootScope.links=[{
-        name:'Bemutatkozas',
-        url:'/home'
-    },
-    {
-        name:'Munkáink',
-        url:'/referncies'
-    },
-    {
-        name:'Szolgaltatsok',
-        url:'/services'
-    },
-    {
-        name:'Kapcsolat',
-        url:'/contacts'
-    }]
+    $rootScope.links = [{
+            name: 'Bemutatkozás',
+            url: '/home'
+        },
+        {
+            name: 'Munkáink',
+            url: '/referencies'
+        },
+        {
+            name: 'Termékek',
+            url: '/products'
+        },
+        {
+            name: 'Szolgáltatásaink',
+            url: '/services'
+        },
+        {
+            name: 'Kapcsolat',
+            url: '/contacts'
+        }
+    ];
 });
 
-app.config(function($routeProvider){
+app.config(function($routeProvider) {
     $routeProvider
-    .when('/home', {
-        templateUrl:'views/home.html',
-        controller:'homectrl'
-    })
-    .when('/referncies', {
-        templateUrl:'views/referncies.html',
-        controller:'refernciesctrl'
-    })
-    .when('/services', {
-        templateUrl:'views/services.html',
-        controller:'servicesctrl'
-    })
-    .when('/contacts', {
-        templateUrl:'views/contacts.html',
-        controller:'contactsctrl'
-    })
-
+        .when('/home', {
+            templateUrl: 'views/home.html',
+            controller: 'homeCtrl'
+        })
+        .when('/referencies', {
+            templateUrl: 'views/referencies.html',
+            controller: 'referenciesCtrl'
+        })
+        .when('/products', {
+            templateUrl: 'views/products.html',
+            controller: 'productsCtrl'
+        })
+        .when('/services', {
+            templateUrl: 'views/services.html',
+            controller: 'servicesCtrl'
+        })
+        .when('/contacts', {
+            templateUrl: 'views/contacts.html',
+            controller: 'contactsCtrl'
+        })
+        .otherwise('/home')
 });
-
---
+++ angular/angular_ngview/js/controllers/referenciesCtrl.js
@@ -0,0 +1,3 @@
+app.controller('referenciesCtrl', function($scope) {
+
+});
