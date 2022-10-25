6c530b43dd717649b6cca96a503e6134fd62f985
zamboilles21 <zamboilles@turr.hu>
2022-10-25 14:10:15 +0200

    dsfsdf

--- angular/angular_ngview/js/controllers/contactsCtrl.js
+++ angular/angular_ngview/js/controllers/contactsCtrl.js
@@ -1,3 +1,16 @@
 app.controller('contactsCtrl', function($scope) {
+    
+    $scope.contacts = [{
+        name: 'Telefon',
+        data:'+36205899852'
+
+        
+    },
+    {
+        name: 'Cim',
+        data:'Valami utca 16'
+    }
+]
+    
 
 });
--- angular/angular_ngview/js/controllers/productsCtrl.js
+++ angular/angular_ngview/js/controllers/productsCtrl.js
@@ -1,3 +1,29 @@
 app.controller('productsCtrl', function($scope) {
-
+    $scope.products = [{
+        name: 'Ásó',
+        price: '5 420',
+        unit: 'Ft',
+        
+    },
+    {
+        name: 'Talicska',
+        price: '15 990',
+        unit: 'Ft'
+    },
+    {
+        name: 'Fejsze nyelezett 1.5 kg JUCO',
+        price: '7 380',
+        unit: 'Ft'
+    },
+    {
+        name: 'Fiskars SmartFit™ Fűnyíró olló',
+        price: '8 890',
+        unit: 'Ft'
+    },
+    {
+        name: 'Kapa bácskai 0.90 kg szögletes NYELEZETT',
+        price: '10 060',
+        unit: 'Ft'
+    }
+]
 });
