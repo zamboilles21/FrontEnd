f28e80a5727ce9c2f379076431d83ece5eca1815
zamboilles21 <zamboilles@turr.hu>
2022-11-14 11:27:13 +0100

    feltolt

--- angular/angular_vizsga/js/app.js
+++ angular/angular_vizsga/js/app.js
@@ -4,6 +4,7 @@ app.run(function($rootScope) {
     $rootScope.title = 'VizsgaAPP';
     $rootScope.author = '2/14. szoftverfejlesztő';
     $rootScope.company = 'Bajai SZC Türr István Technikum';
+    $rootScope.vizsgatipusok=['Záró vizsga','ZH','Részvizsga','Kimeneti vizsga','Belső vizsga']
 
     $rootScope.links = [{
             name: 'Bemutatkozás',
@@ -16,10 +17,27 @@ app.run(function($rootScope) {
         {
             name: 'Kapcsolat',
             url: '/contacts'
-        }
+        },
+        
        
         
     ];
+    $rootScope.ujadat={};
+
+    $rootScope.adatok=angular.fromJson(window.localStorage.getItem('Vizsga'));
+    if($rootScope.adatok==null) $rootScope.adatok=[];
+
+    $rootScope.hozzaadas=function(){
+        if ($rootScope.ujadat.viszganeve == '' || $rootScope.ujadat.vizsgahely == '' || $rootScope.ujadat.vizsgatipus =='' || $rootScope.ujadat.vizsgaidopont=='' ) {
+            $rootScope.message='Nincs minden adat megadva';
+        }else{
+            $rootScope.adatok.push({viszganeve:$rootScope.ujadat.viszganeve,vizsgahely:$rootScope.ujadat.vizsgahely,vizsgatipus:$rootScope.ujadat.vizsgatipus, vizsgaidopont:$rootScope.ujadat.vizsgaidopont });
+            window.localStorage.setItem('Vizsga', angular.toJson($rootScope.adatok));
+            $rootScope.ujadat={};
+        }
+        
+    }
+
 });
 
 app.config(function($routeProvider) {
@@ -38,3 +56,5 @@ app.config(function($routeProvider) {
         })
         .otherwise('/home')
 });
+
+
